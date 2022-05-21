275ded6443166df48f61689273cd90274362cbf8
kawada <vgnsl94@gmail.com>
2022-05-21 23:08:04 +0900

    feat ユーザーupdate機能追加

--- main.go
+++ main.go
@@ -23,6 +23,17 @@ func main() {
 	// fmt.Println(u)
 	// u.CreateUser()
 
-	u, _ := models.GetUser(1)
+	//ユーザー取得
+	u, _ := models.GetUser(2)
 	fmt.Println(u)
+
+	// ユーザー更新
+	u.Name = "test1change"
+	u.Email = "test1@example.com"
+
+	u.UpdateUser()
+
+	u1, _ := models.GetUser(2)
+	fmt.Println(u1)
+
 }
